7b1d87c89f66b44f64122da4746e9493e0393eb9
ManhLuu <manhthe21082003@gmail.com>
2023-06-04 12:06:53 +0700

    fetch: create-api-signup refs: AA-9

---
+++ src/models/auth.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const authSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  role: {
+    type: String,
+    default: "member",
+  },
+});
+export default mongoose.model("Auth", authSchema);
