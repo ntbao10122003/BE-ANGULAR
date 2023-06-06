2d170a5cc0134542b7f3461c4a1aef65e6bca8a0
huy <havu6544@gmail.com>
2023-06-06 21:20:42 +0700

    update checkpermission

--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -1,39 +1,24 @@
-
+import Auth from "../models/auth";
+import { jwt } from "jsonwebtoken";
 export const checkPermission = async (req, res, next) => {
     try {
         if (!req.headers.authorization) {
-            return res.status(403).json({
-                message: "Bạn chưa đăng nhập",
-            });
+          return res.status(403).json({
+            message: "Bạn chưa đăng nhập",
+          });
         }
-        const token = req.headers.authorization.split(" ")[1]; 
-        console.log("token", token);
-        jwt.verify(token, "ManhLD", async (error, payload) => {
-            console.log("error", error);
-            if (error) {
-                if (error.name == "TokenExpiredError") {
-                    return res.json({
-                        message: "Token không hợp lệ",
-                    });
-                }
-                if (error.name == "TokenExpiredError") {
-                    return res.json({
-                        message: "Token hết hạn",
-                    });
-                }
-            }
-            const auth = await Auth.findById(payload.id);
-            console.log(auth);
-            if (!auth && auth.role !== "admin") {
-                return res.status(403).json({
-                    message: "Bạn không có quyền truy cập tài nguyên!",
-                });
-            }
-            next();
-        });
-    } catch (error) {
-        return res.status(401).json({
-            message: error.message || "Token không hợp lệ",
-        });
-    }
+        const token = req.headers.authorization.split(" ")[1];
+        const { id } = jwt.verify(token, "ManhLD");
+    
+        const user = await Auth.findById(id);
+        if (user.role !== "admin") {
+          return res.status(403).json({
+            message: "Bạn không có quyền truy cập!",
+          });
+        }
+        next();
+      } catch (error) {
+    
+        
+      }
 };
